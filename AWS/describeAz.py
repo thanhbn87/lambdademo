36b26abe13d65afa58866fb8902409113a966127
thanhbn <thanhbn87@outlook.com>
2018-09-28 10:42:45 +0700

    add SAM temp and CodeBuild spec

--- AWS/describeAz.py
+++ AWS/describeAz.py
@@ -2,6 +2,6 @@ import boto3
 import json
 
 ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
+def handler(event, context):
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
